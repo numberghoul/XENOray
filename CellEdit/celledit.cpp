3246bc111ef616d8e0292c86a9d44fb0ec35deb1
Logan Williamson <williamson.logan@gmail.com>
2017-04-08 18:53:00 -0700

    New level

--- CellEdit/celledit.cpp
+++ CellEdit/celledit.cpp
@@ -3,22 +3,22 @@
 int Menu()
 {
 
-  int input = 0;
-  cout << "CellEdit v1.0\nby Logan Williamson\n\n";
-  cout << "1) Start\n";
-  cout << "2) Exit\n";
+	int input = 0;
+	cout << "CellEdit v1.0\nby Logan Williamson\n\n";
+	cout << "1) Start\n";
+	cout << "2) Exit\n";
 
-  while(input < 1 || input > 2)
-  {
-    cin >> input;
-  }
+	while(input < 1 || input > 2)
+	{
+		cin >> input;
+	}
 
-  if(input == 1)
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
+	if(input == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
