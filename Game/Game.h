d4b336cc1e37457ba5ad4d6369c2b332fbcfdee0
Logan Williamson <williamson.logan@gmail.com>
2017-04-08 23:56:58 -0700

    Sprites

--- Game/Game.h
+++ Game/Game.h
@@ -9,13 +9,15 @@ using namespace QuickCG;
 #define mapWidth 30
 #define mapHeight 30
 
-#define screenWidth 600
+#define screenWidth 800
 #define screenHeight 480
 
 #define texWidth 24
 #define texHeight 24
 
 #define numTextures 2
+
+#define numSprites 1
 
 class Game
 {
@@ -35,9 +37,18 @@ private:
 	int mScreenWidth, mScreenHeight;
 	std::vector<Uint32> mTextures[numTextures];
 	Uint32 mBuffer[screenHeight][screenWidth];
+	//1D Zbuffer
+	double mZBuffer[screenWidth];
+	//arrays used to sort the sprites
+	int spriteOrder[numSprites];
+	double spriteDistance[numSprites];
+
+	//function used to sort the sprites
+	void combSort(int* order, double* dist, int amount);
 
 	void LoadTextures();
 	void Render(int worldMap[][mapHeight]);
+	void DrawSprites();
 
 	void UpdateMovement(int worldMap[][mapHeight]);
 	void UpdateRotation(float deltaMouse);
