4071f25ae7bcee22e3403e0cf49237d08b801e02
Logan <williamson.logan@gmail.com>
2017-04-20 19:28:54 -0700

    Working button

--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
 #include "Menu/Menu.h"
-#include "Game/Game.h"
 
 int main(int /*argc*/, char */*argv*/[])
 {
 	screen(screenWidth, screenHeight, 0, GAMENAME);
 
 	DisplayMenu();
-
-	screen(screenWidth, screenHeight, 1, GAMENAME);
-
-	DisplayMenu();
-
-	Game game;
-	game.RunGame("e1m1");
 
 	return 0;
 }
